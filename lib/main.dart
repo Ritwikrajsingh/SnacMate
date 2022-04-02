6227dfffdefcfdced912e6cc504dc2fd3a67f353
Dipanshu Shukla <shuklascientist5@gmail.com>
2022-04-02 13:55:18 +0530

    web build added

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,28 @@
 import 'package:firebase_core/firebase_core.dart';
-//import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:snacmate/services/card_provider.dart';
 import 'package:snacmate/services/google_sign_in.dart';
 import 'package:snacmate/pages/home/home.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 
 
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+        apiKey: "AIzaSyCuQyUTpbqqRlvPhVauWOocGAI8yR_pk1E",
+        authDomain: "snacmate.firebaseapp.com",
+        databaseURL: "https://snacmate-default-rtdb.firebaseio.com",
+        projectId: "snacmate",
+        storageBucket: "snacmate.appspot.com",
+        messagingSenderId: "268312469896",
+        appId: "1:268312469896:web:6d8b3a2dcae29f56b65a3d",
+        measurementId: "G-2CT3Z3DBNV"
+    )
+  );
   runApp(const MyApp());
 }
 
